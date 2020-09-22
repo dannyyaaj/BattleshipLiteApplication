dfcde32b2bc0f01877fa3812eaf09ba85e5211dd
Danny Yang <dannyyaaj@gmail.com>
2020-09-21 20:29:45 -0500

    Add models

---
+++ BattleshipLiteLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+    class GridSpotModel
+    {
+        public string SpotLetter { get; set; }
+        public int SpotNumber { get; set; }
+        public GridspotStatus Status { get; set; } = GridspotStatus.Empty; //initial properties
+    }
+}
